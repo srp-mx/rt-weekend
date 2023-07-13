2d3be6fdbc509b15e19eb9eb6dc4a1f702eb88a8
Santiago Romero <srp.devel@gmail.com>
2023-07-13 15:48:39 -0600

    Test PPM

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    // Image
+    const IMAGE_WIDTH:i32 = 256;
+    const IMAGE_HEIGHT:i32 = 256;
+
+    // Render
+    print!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255\n");
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            let r:f64 = (i as f64) / ((IMAGE_WIDTH-1) as f64);
+            let g:f64 = (j as f64) / ((IMAGE_HEIGHT-1) as f64);
+            let b:f64 = 0.25f64;
+
+            let ir:i32 = (255.999f64 * r) as i32;
+            let ig:i32 = (255.999f64 * g) as i32;
+            let ib:i32 = (255.999f64 * b) as i32;
+
+            print!("{ir} {ig} {ib}\n");
+        }
+    }
 }
