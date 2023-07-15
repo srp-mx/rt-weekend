ebe1d3317fc4abc69673632a48e27632af6a88b7
Santiago Romero <srp.devel@gmail.com>
2023-07-14 19:02:48 -0600

    Metal borroso

--- src/main.rs
+++ src/main.rs
@@ -42,8 +42,8 @@ fn main() {
 
     let mat_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
     let mat_center = Rc::new(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
-    let mat_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8)));
-    let mat_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2)));
+    let mat_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
+    let mat_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
 
     let ground_sphere: Rc<dyn Hittable> = Rc::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, mat_ground.clone()));
     let center_sphere: Rc<dyn Hittable> = Rc::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, mat_center.clone()));
--- src/metal.rs
+++ src/metal.rs
@@ -1,3 +1,4 @@
+use super::float::Float;
 use super::rng_float::RngGen;
 use super::material::{Material, Scatter};
 use super::ray::Ray;
@@ -6,19 +7,21 @@ use super::vec3::Vec3;
 use super::color::Color;
 
 pub struct Metal {
-    albedo: Color
+    albedo: Color,
+    fuzz: Float,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
-        Self { albedo }
+    pub fn new(albedo: Color, fuzz: Float) -> Self {
+        Self { albedo, fuzz: if fuzz < 1.0 { fuzz } else { 1.0 } }
     }
 }
 
 impl Material for Metal {
-    fn scatter(&self, r_in: &Ray, hit: &HitRecord, _rng: &mut RngGen) -> Scatter {
+    fn scatter(&self, r_in: &Ray, hit: &HitRecord, rng: &mut RngGen) -> Scatter {
         let reflect = Vec3::reflect(&r_in.direction().unit_vector(), hit.normal());
-        let new_ray = Ray::new(hit.p(), &reflect);
+        let fuzz_offset = self.fuzz * &Vec3::random_sphere(rng);
+        let new_ray = Ray::new(hit.p(), &(&reflect + fuzz_offset));
         return if Vec3::dot(new_ray.direction(), hit.normal()) > 0.0 {
             Scatter::Some(new_ray, self.albedo.copy())
         } else {
