531ada69d13994612dcf9c1a65bdc43aece03518
Santiago Romero <srp.devel@gmail.com>
2023-07-15 01:38:35 -0600

    Material de vidrio

--- src/dielectric.rs
+++ src/dielectric.rs
@@ -14,10 +14,17 @@ impl Dielectric {
     pub fn new(ior: Float) -> Self {
         Self { ior }
     }
+
+    fn reflectance(cosine: Float, refraction_ratio: Float) -> Float {
+        // NOTE(srp): Aproximación de Schlick
+        let r0 = (1.0-refraction_ratio) / (1.0+refraction_ratio);
+        let r0 = r0 * r0;
+        r0 + (1.0 - r0)*(1.0 - cosine).powi(5)
+    }
 }
 
 impl Material for Dielectric {
-    fn scatter(&self, r_in: &Ray, hit: &HitRecord, _rng: &mut RngGen) -> Scatter {
+    fn scatter(&self, r_in: &Ray, hit: &HitRecord, rng: &mut RngGen) -> Scatter {
         let new_color = Color::one();
         let refraction_ratio = if hit.front_face {
             self.ior.recip()
@@ -29,7 +36,10 @@ impl Material for Dielectric {
         let cos_theta = Vec3::dot(&-unit_direction, hit.normal()).min(1.0);
         let sin_theta = (1.0 - cos_theta*cos_theta).sqrt();
 
-        let direction: Vec3 = if refraction_ratio * sin_theta > 1.0 {
+        let cannot_refract: bool = refraction_ratio * sin_theta > 1.0;
+        let reflect: bool = Self::reflectance(cos_theta, refraction_ratio) > rng.get();
+
+        let direction: Vec3 = if cannot_refract || reflect {
             unit_direction.reflect(hit.normal())
         } else {
             unit_direction.refract(hit.normal(), refraction_ratio)
